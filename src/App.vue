20ff0c50c10ede203a2a35a23ebd0410420c1d82
HK-hub <3161880795@qq.com>
2023-09-28 22:58:13 +0800

    feat: 集成路由，layUI,混入，

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script setup>
+import {ref, reactive} from 'vue';
+import {layer} from '@layui/layer-vue'
 
+const model = reactive({})
+
+const submit = () => {
+    layer.msg(`${JSON.stringify(model)}`, {time: 2000});
+};
 </script>
 
 <template>
+    <lay-notice-bar text="以创造性的行为实践于人世。若能以写作为工具，为道途，先帮助自己一程，再以领悟帮助他人一程。这是一种服务。" mode="link"></lay-notice-bar>
+
     <h1>Hello App!</h1>
+    <p>
+        <button @click="">消息提示</button>
+    </p>
+    <p>
+        <lay-button>默认按钮</lay-button>
+        <lay-button type="primary">原始按钮</lay-button>
+        <lay-button type="normal">百搭按钮</lay-button>
+        <lay-button type="warm">暖色按钮</lay-button>
+
+        <lay-button type="danger">警告按钮</lay-button>
+    </p>
     <p>
         <!--使用 router-link 组件进行导航 -->
         <!--通过传递 `to` 来指定链接 -->
@@ -14,6 +34,23 @@
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
+    <div style="width: 80%">
+        <lay-form :model="model">
+            <lay-form-item label="账户" prop="username">
+                <lay-input v-model="model.username"></lay-input>
+            </lay-form-item>
+            <lay-form-item label="密码" prop="password">
+                <lay-input v-model="model.password" type="password"></lay-input>
+            </lay-form-item>
+            <lay-form-item label="描述" prop="desc">
+                <lay-textarea placeholder="请输入描述" v-model="model.describe"></lay-textarea>
+            </lay-form-item>
+            <lay-form-item style="text-align: center;">
+                <lay-button type="primary" @click="submit">提交</lay-button>
+                <lay-button type="default" @click="">重置</lay-button>
+            </lay-form-item>
+        </lay-form>
+    </div>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import {createApp} from 'vue'
 import '@/styles/index.scss'
 import App from './App.vue'
 import router from '@/router'
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(Layui)
 app.mount('#app')
